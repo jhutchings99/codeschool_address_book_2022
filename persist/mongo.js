b66280437796d986ba889978fb5a7592ad2d1c06
Jeremy Hutchings <79938993+jhutchings99@users.noreply.github.com>
2022-06-24 10:05:00 -0600

    set up mongo connect and handlers

---
+++ persist/mongo.js
@@ -0,0 +1,32 @@
+// Pull in mongoose
+const mongoose = require("mongoose");
+
+const db = mongoose.connection;
+
+// Connect to mongoose
+function connect() {
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+// Set up connection handlers
+function setUpConnectionHandlers(callback) {
+    db.once("connecting", () => {
+        console.log("Connecting to MongoDB");
+    });
+
+    db.once("connected", () => {
+        console.log("Connected to MongoDB");
+    });
+
+    db.once("open", () => {
+        console.log("Open Connection to MongoDB");
+        callback();
+    });
+
+    db.once("error", () => {
+        console.log("Error Connecting to MongoDB");
+    });
+}
