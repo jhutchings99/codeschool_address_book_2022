68c89a32b6cc87b5474d287e8caddcf9cc8cb181
Jeremy Hutchings <79938993+jhutchings99@users.noreply.github.com>
2022-06-24 10:22:05 -0600

    stubbed out all of the server methods

--- server.js
+++ server.js
@@ -2,4 +2,30 @@
 const express = require("express");
 const app = express();
 
+// Stub out the methods
+// get address book by id
+app.get("/addressbook/:id", (req, res) => {
+    res.send("get address book by id");
+});
+
+// get all address books
+app.get("/addressbooks", (req, res) => {
+    res.send("get all address books");
+});
+
+// create address book
+app.post("/addressbook", (req, res) => {
+    res.send("create address book");
+});
+
+// update address book
+app.put("/addressbook/:id", (req, res) => {
+    res.send("update address book");
+});
+
+// delete address book
+app.delete("/addressbook/:id", (req, res) => {
+    res.send("delete address book");
+});
+
 module.exports = app;
